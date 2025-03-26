7dcb9398de641a0bae64c29f4d5cda2aaf5050c6
unknown <239047@TECH.WYB.AC.LK>
2025-03-26 20:12:46 +0530

    first commit

--- Q5.py
+++ Q5.py
@@ -2,4 +2,3 @@ x=int(input("enter num 1"))
 y=int(input("enter num 2"))
 print((x+y)/2)
 
-
---
+++ Q6.py
@@ -0,0 +1,7 @@
+num=2
+if(num>5):
+    print("print is grater 5")
+elif num>10:
+    print("print is grater 10")
+else:
+    print("print is grater")
